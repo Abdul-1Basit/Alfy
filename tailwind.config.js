ffeac34722b66330f6e9c64d9bd6885ae13c8720
Abdul Basit <muhammad.zubair3@hsrw.org>
2022-08-19 01:06:41 +0500

    p to P c to C

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./Pages/**/*.{js,ts,jsx,tsx}",
+		"./Components/**/*.{js,ts,jsx,tsx}",
 	],
-	purge: [
-		"./src/**/*.{js,ts,jsx,tsx}",
-		// Add more here
-	],
+
 	theme: {
 		screens: {
 			xs: "375px",
